de227ac57da348db3d127aeac5624d3a8b327a9d
mahngueloh <mahnguelohzack@gmail.com>
2026-03-21 02:55:11 +0300

    Create antivoice.js to block voice messages.

---
+++ antivoice.js
@@ -0,0 +1,10 @@
+// antivoice.js
+
+// This code prevents users from sending voice messages in the group.
+
+function blockVoiceMessages() {
+    // Logic to block voice messages
+    console.log('Voice messages are blocked.');
+}
+
+blockVoiceMessages();
