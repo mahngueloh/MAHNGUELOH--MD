5e8c7fbf9b0e4aa6fe581c63cef0e477ed60df2c
mahngueloh <mahnguelohzack@gmail.com>
2026-03-21 02:57:56 +0300

    Create ban.js file to implement user banning functionality

---
+++ ban.js
@@ -0,0 +1,10 @@
+// ban.js
+
+// Function to ban users from groups
+function banUser(groupId, userId) {
+    // Logic to ban the user
+    console.log(`User ${userId} has been banned from group ${groupId}.`);
+}
+
+// Example usage
+// banUser('group123', 'user456');
