fa669207472c1f814dc246631ca3c87065b9c52b
mahngueloh <mahnguelohzack@gmail.com>
2026-03-21 02:48:23 +0300

    Create commands.js with anti-commands

---
+++ commands.js
@@ -0,0 +1,38 @@
+// Commands for Discord Bot
+
+const antigroupmention = {
+    name: 'antigroupmention',
+    execute(message) {
+        // implementation code
+    }
+};
+
+const antilink = {
+    name: 'antilink',
+    execute(message) {
+        // implementation code
+    }
+};
+
+const antisticker = {
+    name: 'antisticker',
+    execute(message) {
+        // implementation code
+    }
+};
+
+const antibadword = {
+    name: 'antibadword',
+    execute(message) {
+        // implementation code
+    }
+};
+
+const antidelete = {
+    name: 'antidelete',
+    execute(message) {
+        // implementation code
+    }
+};
+
+module.exports = { antigroupmention, antilink, antisticker, antibadword, antidelete };
